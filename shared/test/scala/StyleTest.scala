af3bbe01348fc4edaf355f569ae6c8e8966e28ae
maeda <maeda@illi-ichi.com>
2014-04-14 14:37:32 +0900

    add tests for another size of display

--- shared/test/scala/StyleTest.scala
+++ shared/test/scala/StyleTest.scala
@@ -4,48 +4,91 @@ import model.Screen.{Point, Center}
 
 object StyleTest extends TestSuite{
   def tests = TestSuite {
-    val screen = (1000, 1000)
-    val conv = new Screen.Converter(screen._1, screen._2)
-    val fig = Figure("hoge", 0.5)
+    "screen_1000x1000" -{
+      val screen = (1000, 1000)
+      val conv = new Screen.Converter(screen._1, screen._2)
+      val fig = Figure("hoge", 0.5)
 
-    "none" -{
-      val (id, style) = fig.hide(conv)
-      assert(style == "display:none;")
-    } 
+      "none" -{
+        val (id, style) = fig.hide(conv)
+        assert(style == "display:none;")
+      }
 
-    val l  = Point(Center.point, 0.5)
+      val l  = Point(Center.point, 0.5)
 
-    "left_top" -{
-      val (id, style) = fig.style(l, 0.3)(conv)
-      assert(
-        style.contains("width:300px;"),
-        style.contains("height:150px;"),
-        style.contains("top:500px;"),
-        style.contains("left:500px;")
-      )
+      "left_top" -{
+        val (id, style) = fig.style(l, 0.3)(conv)
+        assert(
+          style.contains("width:300px;"),
+          style.contains("height:150px;"),
+          style.contains("top:500px;"),
+          style.contains("left:500px;")
+        )
+      }
+
+      "center" -{
+        val (id, style) = fig.style(l, 0.6, origin = Figure.Center)(conv)
+        assert(
+          style.contains("width:600px;"),
+          style.contains("height:300px;"),
+          style.contains("top:350px;"),
+          style.contains("left:200px;")
+        )
+      }
+      "right" -{
+        val (id, style) = fig.style(l, 0.3, origin = Figure.RightBottom)(conv)
+        assert(
+          style.contains("width:300px;"),
+          style.contains("height:150px;"),
+          style.contains("top:350px;"),
+          style.contains("left:200px;")
+        )
+      }
     }
 
-    "center" -{
-      val (id, style) = fig.style(l, 0.6, origin = Figure.Center)(conv)
-      assert(
-        style.contains("width:600px;"),
-        style.contains("height:300px;"),
-        style.contains("top:350px;"),
-        style.contains("left:200px;")
-      )
-    }
-    "right" -{
-      val (id, style) = fig.style(l, 0.3, origin = Figure.RightBottom)(conv)
-      assert(
-        style.contains("width:300px;"),
-        style.contains("height:150px;"),
-        style.contains("top:350px;"),
-        style.contains("left:200px;")
-      )
+    "screen_1500x1000" -{
+      val screen = (1500, 1000)
+      val conv = new Screen.Converter(screen._1, screen._2)
+      val fig = Figure("hoge", 0.5)
+
+      "none" -{
+        val (id, style) = fig.hide(conv)
+        assert(style == "display:none;")
+      }
+
+      val l  = Point(Center.point, 0.5)
+
+      "left_top" -{
+        val (id, style) = fig.style(l, 0.3)(conv)
+        assert(
+          style.contains("width:300px;"),
+          style.contains("height:150px;"),
+          style.contains("top:500px;"),
+          style.contains("left:750px;")
+        )
+      }
+
+      "center" -{
+        val (id, style) = fig.style(l, 0.6, origin = Figure.Center)(conv)
+        assert(
+          style.contains("width:600px;"),
+          style.contains("height:300px;"),
+          style.contains("top:350px;"),
+          style.contains("left:450px;")
+        )
+      }
+      "right" -{
+        val (id, style) = fig.style(l, 0.3, origin = Figure.RightBottom)(conv)
+        assert(
+          style.contains("width:300px;"),
+          style.contains("height:150px;"),
+          style.contains("top:350px;"),
+          style.contains("left:450px;")
+        )
+      }
     }
   }
 }
-
 
 /*
  style:"transform: rotateX(60deg) rotateY(10deg) perspective(100px);"
