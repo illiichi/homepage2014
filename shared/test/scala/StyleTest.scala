13ff702be575e2d4a90d6bf14f2f1f00c9196655
maeda <maeda@illi-ichi.com>
2014-04-14 18:21:12 +0900

    add vertical display case

--- shared/test/scala/StyleTest.scala
+++ shared/test/scala/StyleTest.scala
@@ -87,6 +87,48 @@ object StyleTest extends TestSuite{
         )
       }
     }
+
+    "screen_1000x1500" -{
+      val screen = (1000, 1500)
+      val conv = new Screen.Converter(screen._1, screen._2)
+      val fig = Figure("hoge", 0.5)
+
+      "none" -{
+        val (id, style) = fig.hide(conv)
+        assert(style == "display:none;")
+      }
+
+      val l  = Point(Center.point, 0.5)
+
+      "left_top" -{
+        val (id, style) = fig.style(l, 0.3)(conv)
+        assert(
+          style.contains("width:300px;"),
+          style.contains("height:150px;"),
+          style.contains("top:750px;"),
+          style.contains("left:500px;")
+        )
+      }
+
+      "center" -{
+        val (id, style) = fig.style(l, 0.6, origin = Figure.Center)(conv)
+        assert(
+          style.contains("width:600px;"),
+          style.contains("height:300px;"),
+          style.contains("top:600px;"),
+          style.contains("left:200px;")
+        )
+      }
+      "right" -{
+        val (id, style) = fig.style(l, 0.3, origin = Figure.RightBottom)(conv)
+        assert(
+          style.contains("width:300px;"),
+          style.contains("height:150px;"),
+          style.contains("top:600px;"),
+          style.contains("left:200px;")
+        )
+      }
+    }
   }
 }
 
