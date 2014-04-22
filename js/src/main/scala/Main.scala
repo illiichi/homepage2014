ea959cfd222a5986e2ef54fb5a024b1ff98925eb
maeda <maeda@illi-ichi.com>
2014-04-22 23:05:00 +0900

    separate complete action from window size

--- js/src/main/scala/Main.scala
+++ js/src/main/scala/Main.scala
@@ -6,29 +6,33 @@ import rx.ops._
 @JSExport
 object Main {
   val page = new definition.IlliIchiPage with view.BrowserViewAction
-  import page.parser.InputState
+  import page.parser._
 
   @JSExport
   def main(): Unit = {
     val view = new _root_.view.View()
 
     val inputState = view.menuText.map(page.parser.parse)
-    val styles = Rx { mainFlow(inputState(), view.screenSize()) }
+
+    inputState.foreach( { 
+      case Complete((_, action)) => action()
+      case _ =>
+    }
+    , skipInitial = true)
+
+    val styles = Rx { toStyleAttribute(inputState(), view.screenSize()) }
 
     view.start(styles)
   }
 
-  def mainFlow(inputState: InputState, screenSize: (Int, Int)):Map[String, String] = {
-    import page.parser._
+  def toStyleAttribute(inputState: InputState, screenSize: (Int, Int)):Map[String, String] = {
     val converter = new logic.Screen.Converter(screenSize)
 
     (inputState match {
       case NoInput => page.defaultStyles
-      case DuringInput((effect, _), count) => effect(Some(count))
-      case Complete((effect, action)) => 
-        action()
-        effect(None)
       case WrongInput => page.illegalStyles
+      case DuringInput((effect, _), count) => effect(Some(count))
+      case Complete((effect, _)) => effect(None)
     }).map{ f => f(converter) }.toMap
   }
 }
