0296bf7a6af16a755eb2f43189a3aa1e2b4f8f71
maeda <maeda@illi-ichi.com>
2014-04-24 09:28:01 +0900

    add shell to prepare gh pages

---
+++ copy.sh
@@ -0,0 +1,11 @@
+for file in index-dev.html index.html style.css images/buddha01.svg images/fish.svg images/fish02.svg images/fukusuke1.svg images/fukusuke2.svg images/fukusuke3.svg images/illi-ichi_kanji.svg images/illi-ichi_logo.svg images/lambda-cube.svg images/menu.svg images/name.svg images/nami.svg target/scala-2.10/js-opt.js ; do
+    cp js/$file ../illi-ichi.github.io/$file
+done
+
+hash=$(cat .git/refs/heads/master)
+
+cd ../illi-ichi.github.io
+git add .
+git commit -m $hash
+
+cd -
