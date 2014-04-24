9b6d91b3af15914ee0c4be318432993303d05a39
maeda <maeda@illi-ichi.com>
2014-04-25 00:27:47 +0900

    optimize js before commit

--- copy.sh
+++ copy.sh
@@ -1,3 +1,5 @@
+sbt "; project js ; optimizeJS"
+
 for file in index-dev.html index.html style.css images/buddha01.svg images/fish.svg images/fish02.svg images/fukusuke1.svg images/fukusuke2.svg images/fukusuke3.svg images/illi-ichi_kanji.svg images/illi-ichi_logo.svg images/lambda-cube.svg images/menu.svg images/name.svg images/nami.svg target/scala-2.10/js-opt.js ; do
     cp js/$file ../illi-ichi.github.io/$file
 done
