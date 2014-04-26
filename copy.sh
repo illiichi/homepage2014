f779d4d4ff488672ae6b6046a51a5982d485df04
maeda <maeda@illi-ichi.com>
2014-04-27 05:23:14 +0900

    add favicon

--- copy.sh
+++ copy.sh
@@ -1,8 +1,10 @@
 sbt "; project js ; optimizeJS"
 
-for file in index-dev.html index.html style.css images/buddha01.svg images/fish.svg images/fish02.svg images/fukusuke1.svg images/fukusuke2.svg images/fukusuke3.svg images/illi-ichi_kanji.svg images/illi-ichi_logo.svg images/lambda-cube.svg images/menu.svg images/name.svg images/nami.svg images/tooltip.svg target/scala-2.10/js-opt.js ; do
+for file in index-dev.html index.html style.css images/buddha01.svg images/fish.svg images/fish02.svg images/fukusuke1.svg images/fukusuke2.svg images/fukusuke3.svg images/illi-ichi_kanji.svg images/illi-ichi_logo.svg images/lambda-cube.svg images/menu.svg images/name.svg images/nami.svg images/tooltip.svg target/scala-2.10/js-opt.js; do
     cp js/$file ../illi-ichi.github.io/$file
 done
+
+cp js/images/favicon.ico ../illi-ichi.github.io/favicon.ico
 
 hash=$(cat .git/refs/heads/master)
 
